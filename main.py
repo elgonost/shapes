38a4dfb83587ba25e4f0c88b02bf86bdc4bf72c6
elgonost <kostaspetsasx@gmail.com>
2023-03-23 15:14:37 +0200

    Restructure files

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+from shapes.hashtag import HashtagTriangle, HashtagSquare
+
 if __name__ == "__main__":
+
+    ht = HashtagTriangle(5)
+    print(ht)
+
+    hs = HashtagSquare(5)
+    print(hs)
+
     pass
